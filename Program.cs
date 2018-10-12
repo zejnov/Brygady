3d02503835b6f7f7b60e993a5d4bc20ed6ee0120
Mateusz ZejnoV <Zejnov@gmail.com>
2018-10-12 13:52:15 +0200

    Update Program.cs
    extra line removal

--- Program.cs
+++ Program.cs
@@ -17,7 +17,6 @@ namespace Brygady2
             CreateWebHostBuilder(args).Build().Run();
         }
 
-
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseUrls("http://0.0.0.0:5001")
